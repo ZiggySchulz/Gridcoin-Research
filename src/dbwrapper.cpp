9e3b34ade25cfaa8bdda1c13460b78de00de2118
Ziggy Schulz <ziggyschulz@gmail.com>
2025-05-02 23:01:34 +1000

    Splash screen connection

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -420,6 +420,8 @@ bool CTxDB::LoadBlockIndex()
                     _("Blocks Loaded"),
                     (100 * nLoaded / nHighest)));
 
+                uiInterface.InitProgress(nLoaded, nHighest);
+
                 tfm::format(std::cout,"%d ",nLoaded); fflush(stdout);
             }
         }
@@ -473,6 +475,7 @@ bool CTxDB::LoadBlockIndex()
             if ((nCurrentDepth % 1000) == 0)
             {
                 uiInterface.InitMessage(strprintf("%" PRId64 "/%" PRId64 " %s", nCurrentDepth, nCheckDepth, _("Blocks Verified")));
+                uiInterface.InitProgress(nLoaded, nHighest);
             }
         }
 
